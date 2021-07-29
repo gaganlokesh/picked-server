a9c47a53be01967e2152535cf73aa8b5e88ba96c
Gagan Lokesh <gaganlokesh@gmail.com>
2021-07-29 23:39:53 +0530

    Add new users controller
    - Add global `current_user` method to get logged-in user record using doorkeeper
    
    - Update routes for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::API
   def not_authorized
     render json: { message: "Unauthorized Request" }, status: :unauthorized
   end
+
+  def current_user
+    return current_resource_owner if doorkeeper_token
+  end
+
+  private
+
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
 
   resources :articles, only: [:index]
   post "articles/webhook", to: "articles#webhook"
+  resources :users, only: [:show] do
+    collection do
+      get :me
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
