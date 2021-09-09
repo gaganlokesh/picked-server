9ea1efa647416d38b9e1b2c12d5b0b827c68cf61
Gagan Lokesh <gaganlokesh@gmail.com>
2021-09-09 19:40:40 +0530

    Add new `image_url` field to source response
    - Use a placeholder value for now for development purposes

--- app/blueprints/source_blueprint.rb
+++ app/blueprints/source_blueprint.rb
@@ -1,3 +1,9 @@
 class SourceBlueprint < ApplicationBlueprint
   fields :id, :name, :website_url, :slug
+
+  field :image_url do
+    # FIXME: Returning dummy image URL for development purposes
+    # Replace this with appropriate S3 URL
+    "https://picked-dev.s3.ap-south-1.amazonaws.com/sources/logo/ycombinator-logo.png"
+  end
 end
