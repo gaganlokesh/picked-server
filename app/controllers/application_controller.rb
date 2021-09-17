a8ab42ac82f07fc843465944a4ff55f1bb8e0ad5
Gagan Lokesh <gaganlokesh@gmail.com>
2021-09-17 23:52:39 +0530

    Handle ActiveRecord errors with appropriate responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,27 @@
 class ApplicationController < ActionController::API
   before_action :doorkeeper_authorize!
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    unprocessable_entity(e.message)
+  end
+
   def not_authorized
-    render json: { message: "Unauthorized Request" }, status: :unauthorized
+    render json: { status: 401, error: "Unauthorized Request" }, status: :unauthorized
+  end
+
+  def not_found
+    render json: { status: 404, error: "Not Found" }, status: :not_found
+  end
+
+  def unprocessable_entity(message)
+    render json: { status: 422, error: message }, status: :unprocessable_entity
+  end
+
+  # Custom doorkeeper authorization error response
+  def doorkeeper_unauthorized_render_options(*)
+    { json: { status: 401, error: "Unauthorized Request" } }
   end
 
   def current_user
