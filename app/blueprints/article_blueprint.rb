49a89c8b92071983d1317fe0450d86d6dfae2f96
Gagan Lokesh <gaganlokesh@gmail.com>
2021-11-11 04:17:42 +0530

    Include article reactions fields in response object

--- app/blueprints/article_blueprint.rb
+++ app/blueprints/article_blueprint.rb
@@ -1,6 +1,8 @@
 class ArticleBlueprint < ApplicationBlueprint
   fields :id, :title, :url, :canonical_url, :image_placeholder, :read_time, :published_at
 
+  field :reactions_count, name: :upvotes_count
+
   field :image_url do
     # FIXME: Returning dummy image URL for development purposes
     # Replace this with appropriate S3 URL
@@ -11,6 +13,11 @@ class ArticleBlueprint < ApplicationBlueprint
     field :is_bookmarked do |article, options|
       # FIXME: This operation will be expensive if there are a lot of bookmarked users for an article
       article.bookmarks.pluck(:user_id).include?(options[:current_user]&.id)
+    end
+
+    field :is_upvoted do |article, options|
+      # FIXME: This operation will be expensive if there are a lot of reactions for an article
+      article.reactions.pluck(:user_id).include?(options[:current_user]&.id)
     end
   end
 
