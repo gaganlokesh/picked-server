ff839a8406cb31e4c41317db57babe7626189a85
Gagan Lokesh <gaganlokesh@gmail.com>
2021-11-16 00:06:38 +0530

    Discard score update job if article doesn't exist

--- app/jobs/articles/score_update_job.rb
+++ app/jobs/articles/score_update_job.rb
@@ -2,6 +2,8 @@ module Articles
   class ScoreUpdateJob < ApplicationJob
     queue_as :default
 
+    discard_on ActiveRecord::RecordNotFound
+
     def perform(article_id)
       article = Article.find(article_id)
       article.update_score
