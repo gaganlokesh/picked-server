ff62546f637992e973e0bf254e22a17aa2f4bf6f
Gagan Lokesh <gaganlokesh@gmail.com>
2022-02-05 21:52:49 +0530

    Update user blueprint to include imgproxy URL for profile images

--- app/blueprints/user_blueprint.rb
+++ app/blueprints/user_blueprint.rb
@@ -1,3 +1,7 @@
 class UserBlueprint < ApplicationBlueprint
-  fields :id, :name, :email, :profile_image_url
+  fields :id, :name, :email
+
+  field :profile_image_url do |user, _|
+    ImageOptimizer::S3.call(user.profile_image.path, width: 150, height: 150) if user.profile_image.present?
+  end
 end
