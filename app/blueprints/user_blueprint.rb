0d056d1c17cf22fb0dba5425641063e010737db5
Gagan Lokesh <gaganlokesh@gmail.com>
2022-02-21 00:33:55 +0530

    Include `username` property in user response

--- app/blueprints/user_blueprint.rb
+++ app/blueprints/user_blueprint.rb
@@ -1,5 +1,5 @@
 class UserBlueprint < ApplicationBlueprint
-  fields :id, :name, :email
+  fields :id, :name, :email, :username
 
   field :profile_image_url do |user, _|
     ImageOptimizer::S3.call(user.profile_image.path, width: 150, height: 150) if user.profile_image.present?
